9211a1ab3981e356541f3f88fa9251cf29473404
gmegidish <gil@megidish.net>
2025-07-08 10:34:40 +0200

    Initial Commit

---
+++ pricey/priceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  priceyApp.swift
+//  pricey
+//
+//  Created by Gil Megidish on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct priceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
